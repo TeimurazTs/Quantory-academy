7e7b34686d687dcfb6b81252d9f038dccc12f611
TeimurazTs <temotsuladze99@gmail.com>
2023-04-15 19:31:36 +0400

    finish task

--- oop/base_classes.js
+++ oop/base_classes.js
@@ -1,28 +1,23 @@
 class Component {
-    constructor() {
-        this.state = {};
-        this.props = {};
-        this.element = document.createElement('div');
-    }
+  constructor() {
+    this.state = {};
+    this.props = {};
+    this.element = document.createElement("div");
+  }
 
-    setState(state) {
-        this.state = {...this.state, ...state};
-        this.update();
-    }
+  setState(state) {
+    this.state = { ...this.state, ...state };
+  }
 
-    render(props = {}) {
-        this.props = {...props};
-        const div = this.element;
-        div.onclick = props.onClick;
-        if (props.style) {
-            div.style = props.style;
-        }
-        div.innerHTML = '';
-        div.append(...props.children);
-        return div;
+  render(props = {}) {
+    this.props = { ...props };
+    const div = this.element;
+    div.onclick = props.onClick;
+    if (props.style) {
+      div.style = props.style;
     }
-
-    update() {
-        this.render(this.props);
-    }
+    div.innerHTML = "";
+    div.append(...props.children);
+    return div;
+  }
 }
--- oop/components.js
+++ oop/components.js
@@ -1,44 +1,30 @@
-class Button extends Component {
-  constructor() {
-    super();
-    this.element = document.createElement("button");
-    this.element.style.cssText = "color: red;";
-  }
-
-  render(props) {
-    return super.render({
-      onClick: props.onClick,
-      children: props.text,
-      style: this.state.style,
-    });
-  }
-}
-
-class List extends Component {
+class Modal extends Component {
   constructor() {
     super();
     this.element = document.createElement("ul");
   }
 
   render(props) {
-    return super.render({
-      children: [
-        ...props.items.map((item) => {
-          return new ListItem().render({ children: item });
-        }),
-        new Button().render({
-          text: "Add item",
-          onClick: props.addItem,
-        }),
-      ],
-    });
+    return super.render({ props });
   }
 }
 
-class ListItem extends Component {
+class Input extends Component {
+  constructor() {
+    super();
+  }
+
   render(props) {
-    return super.render({
-      children: props.children,
-    });
+    return super.render({ props });
+  }
+}
+
+class Button extends Component {
+  constructor() {
+    super();
+  }
+
+  render(props) {
+    return super.render({ props });
   }
 }
